8755bd840fda7714c711a333fc2c5544d457a71b
Miguel Acevedo Cádiz <macevedoinf@gmail.com>
2021-01-28 15:00:20 -0300

    Adding message prefix

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const { DateTime } = require("luxon");
 
 const client = new Discord.Client();
 
-client.login(config.TOKEN);
+const { TOKEN, SEND_HOURS, CHANNEL, MESSAGE_PREFIX } = config;
+
+client.login(TOKEN);
 
 const shouldSendMessage = () => {
-  return config.SEND_HOURS.includes(DateTime.local().toFormat("HH:mm"));
+  return SEND_HOURS.includes(DateTime.local().toFormat("HH:mm"));
 };
 
 const getMessage = () => {
@@ -22,11 +24,11 @@ const getMessage = () => {
 
 client.on("ready", () => {
   const channel = client.channels.cache.find(
-    (channel) => channel.name === config.CHANNEL && channel.type === "text"
+    (channel) => channel.name === CHANNEL && channel.type === "text"
   );
   setInterval(() => {
     if(shouldSendMessage()) {
-        channel.send(getMessage());
+        channel.send(`${MESSAGE_PREFIX ? MESSAGE_PREFIX : ''} ${getMessage()}`);
     }
   }, 60000);
 });
